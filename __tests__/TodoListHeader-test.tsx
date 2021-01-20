7f1c42dc868f4614782ac294acb2a491702cc79a
Botond Mátyus <matyusbotond@outlook.hu>
2021-01-20 11:07:53 +0100

    Implement TodoListHeader tests

--- __tests__/TodoListHeader-test.tsx
+++ __tests__/TodoListHeader-test.tsx
@@ -2,3 +2,30 @@ import 'react-native';
 import React from 'react';
 import {render, fireEvent} from '@testing-library/react-native';
 import TodoListHeader from '../TodoListHeader';
+
+it('renders correctly', () => {
+  render(<TodoListHeader itemCount={0} newButtonPressed={jest.fn()} />);
+});
+
+it('render items count', () => {
+  const rendered = render(
+    <TodoListHeader itemCount={0} newButtonPressed={jest.fn()} />,
+  );
+
+  const itemCountText = rendered.getByA11yLabel('itemCount');
+
+  expect(itemCountText.props.children).toBe(0);
+});
+
+it('should call newButtonPressed if user clicked new button', () => {
+  const newButtonPressedMock = jest.fn();
+  const rendered = render(
+    <TodoListHeader itemCount={0} newButtonPressed={newButtonPressedMock} />,
+  );
+
+  const newButton = rendered.getByA11yLabel('newButton');
+
+  fireEvent(newButton, 'onPress');
+
+  expect(newButtonPressedMock).toBeCalled();
+});
